b3b98aac8ad14e28518de12ee38ed533135d2646
Mahdi <44380968+mbonyani@users.noreply.github.com>
2021-05-15 12:51:44 +0430

    Update test.py

--- mmseg/apis/test.py
+++ mmseg/apis/test.py
@@ -86,6 +86,7 @@ def eval_metrics(results,
                  reduce_zero_label=False):
 
     num_classes =150
+    ignore_index=False
     if isinstance(metrics, str):
         metrics = [metrics]
     allowed_metrics = ['mIoU', 'mDice']
