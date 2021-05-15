f97637694175e741cfd8c8ca37cd1f62fdcd4eb8
Mahdi <44380968+mbonyani@users.noreply.github.com>
2021-05-15 16:43:55 +0430

    Update test.py

--- mmseg/apis/test.py
+++ mmseg/apis/test.py
@@ -217,6 +217,8 @@ def single_gpu_test(model,
             if i <4:
               print("Metric:",ret_met)
               np.save(str(i)+"a.npy",result[0])
+              np.save(str(i)+"b.npy",seg[0])
+              
             else:
               break
             ret_mets.append(ret_met)
